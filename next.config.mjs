acdaa1a1d43d08a3ff33b2546c9f9a152e24b5af
Peter Fernandez <peter.fernandez@okta.com>
2024-06-19 19:59:28 +0100

    Temporarily disable type checking

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,14 @@ const nextConfig = {
             destination: '/dashboard/organization/members',
             permanent: true
         }]
-    }
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    }    
 };
 
 export default nextConfig;
